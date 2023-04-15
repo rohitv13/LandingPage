f9fc9d3d8cba3309772a237388e4e42014d10bec
rohitv13 <130787985+rohitv13@users.noreply.github.com>
2023-04-15 10:06:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+/* JavaScript code for image slideshow */
+  
+  const images = [
+  "https://www.whats-on-netflix.com/wp-content/uploads/2016/02/netflix_hero.jpg",
+  "https://www.whats-on-netflix.com/wp-content/uploads/2016/02/stranger-things.jpg",
+  "https://www.whats-on-netflix.com/wp-content/uploads/2016/02/narcos.jpg",
+  "https://www.whats-on-netflix.com/wp-content/uploads/2016/02/the-crown.jpg",
+  "https://www.whats-on-netflix.com/wp-content/uploads/2016/02/mindhunter.jpg",
+  ];
+  
+  let index = 0;
+  const slideshow = document
